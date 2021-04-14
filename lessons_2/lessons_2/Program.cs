f8808c8b20ed6a997cb4a016438e6ffa8e91bd5c
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-14 14:57:42 +0300

    Lessons_2

---
+++ lessons_2/lessons_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lessons_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num_1 ;
+            
+            //uint num_1 = 10;
+            //float f = 1.23f;
+            //bool boolean = false;
+            //string text = "Work";
+            //char n = 'W';
+
+
+            Console.WriteLine("Enter first num: ");
+            num = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("\nEnter first num_1: ");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nThe result is: " + (num + num_1).ToString());
+            Console.ReadKey();
+        }
+    }
+}
