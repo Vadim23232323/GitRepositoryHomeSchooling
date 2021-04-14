e4de62ad9ea5ebad18989698935c264f8a038f18
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-14 15:15:14 +0300

    lessons_3

---
+++ lessons_3/lessons_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lessons_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+            Console.WriteLine("How old are you?");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            if (num > 18.23d && num <25.34d)
+                Console.WriteLine("You are older than 18.23");
+            else
+                Console.WriteLine("Else");
+
+            string name = num == 15 ? "First" : "Second";
+            Console.WriteLine(name);
+            
+            Console.ReadKey();
+        }
+    }
+}
