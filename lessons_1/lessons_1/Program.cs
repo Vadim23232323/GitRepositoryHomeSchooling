3a4cd53d599e3f016b37f42b48ed0c811236daa6
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-14 12:05:51 +0300

    Lessons_1

---
+++ lessons_1/lessons_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lessons_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi Hi Hi \nНажмите любую клавишу для закрытия консоли");
+            Console.ReadKey();
+        }
+    }
+}
