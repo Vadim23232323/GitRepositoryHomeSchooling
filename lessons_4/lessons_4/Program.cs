6ea2bde59779500dc8856c5a91e9ec10ce852670
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-14 15:52:15 +0300

    Оператор Switch и операция Random

---
+++ lessons_4/lessons_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lessons_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randNum = new Random();
+
+            int num;
+            Console.WriteLine("How old are you?");
+            num = randNum.Next(0, 6);
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Resultat " + num);
+                    break;
+                case 2:
+                    Console.WriteLine("Resultat " + num);
+                    break;
+                case 3:
+                    Console.WriteLine("Resultat " + num);
+                    break;
+                case 4:
+                    Console.WriteLine("Resultat " + num);
+                    break;
+                case 5:
+                    Console.WriteLine("Resultat " + num);
+                    break;
+                default:
+                    Console.WriteLine("Nothing " + num);
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
