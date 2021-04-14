bc644e6bb1bf9d7ff903187cc33995ce88713737
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-14 15:47:54 +0300

    Оператор условия if

--- lessons_3/lessons_3/Program.cs
+++ lessons_3/lessons_3/Program.cs
@@ -6,33 +6,17 @@ namespace lessons_3
     {
         static void Main(string[] args)
         {
-            Random randNum = new Random();
-
-            int num;
+            double age;
             Console.WriteLine("How old are you?");
-            num = randNum.Next(0, 6);
+            age = Convert.ToDouble(Console.ReadLine());
 
-            switch (num)
-            {
-                case 1: Console.WriteLine("Resultat " +num);
-                    break;
-                case 2:
-                    Console.WriteLine("Resultat " + num);
-                    break;
-                case 3:
-                    Console.WriteLine("Resultat " + num);
-                    break;
-                case 4:
-                    Console.WriteLine("Resultat " + num);
-                    break;
-                case 5:
-                    Console.WriteLine("Resultat " + num);
-                    break;
-                default:
-                    Console.WriteLine("Nothing " + num);
-                    break;
-            }
+            if (age > 18 && age < 23)
+                Console.WriteLine("age match");
+            else
+                Console.WriteLine("age does not match");
 
+            string name = age == 15 ? "first" : "second";
+            Console.WriteLine(name);
             Console.ReadKey();
         }
     }
