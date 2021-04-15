cb871c76bf0d71bfc14d29ac906630bf5fd6dd00
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-15 09:36:22 +0300

    Цикл for, While, do While

---
+++ lessons_5/lessons_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lessons_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0, number=1;
+
+            for (i = 10; i >= 5; i--)
+            {
+                if (i==8)
+                    break;
+                Console.WriteLine(i + "\n");
+            }
+
+            //while (i<=10)
+            //{
+            //    Console.WriteLine(i + "\n");
+            //    i++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine("Enter number:");
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //} while (number != 5);
+
+            Console.ReadKey();
+        }
+    }
+}
