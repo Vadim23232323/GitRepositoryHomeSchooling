2ccec563470a694e85c45902688820b29837c605
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-19 15:42:24 +0300

    Цикл foreach

---
+++ lessons_7/lessons_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lessons_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numbers = {1.23f, 23.23f, 23.57f};
+
+            foreach (var el in numbers)
+            {
+                Console.WriteLine(el);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
