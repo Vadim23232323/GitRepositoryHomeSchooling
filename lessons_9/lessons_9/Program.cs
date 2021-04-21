551465792a31f857b8975a5111c54a7e66dea744
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-21 14:48:00 +0300

    Lessons_8

---
+++ lessons_9/lessons_9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lessons_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_1, num_2, result;
+            OneMetod("HI");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            num_2 = Convert.ToInt32(Console.ReadLine());
+            result=Add(num_1,num_2);
+            Console.WriteLine("Result is - " + result);
+            Console.ReadKey();
+
+        }
+
+        static void OneMetod(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        static int Add(int num_1, int num_2)
+        {
+            return num_1 + num_2;
+            
+        }
+    }
+}
