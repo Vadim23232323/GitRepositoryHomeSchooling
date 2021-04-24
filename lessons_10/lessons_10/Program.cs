9f0f0bec182ecce3772ed936286dbe87393e6db9
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-24 17:47:28 +0300

    lessons_10

---
+++ lessons_10/lessons_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lessons_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello, world, maks";
+            Console.WriteLine(string.IsNullOrEmpty(str));
+            Console.WriteLine(string.IsNullOrWhiteSpace(str));
+
+            String.Compare("rttr", "b"); 
+
+            Console.WriteLine(str.ToUpper());
+            Console.WriteLine(str.ToLower());
+            Console.WriteLine(str.Contains("hel"));
+            Console.WriteLine(str.StartsWith("hel"));
+            Console.WriteLine(str.EndsWith("hel"));
+            Console.WriteLine(str.Insert(5," world"));
+            Console.WriteLine(str.Remove(3));
+            char[] sym = str.ToCharArray();
+            string[] clubs = str.Split (',');
+            Console.WriteLine(clubs[2]);
+            Console.ReadKey();
+        }
+    }
+}
