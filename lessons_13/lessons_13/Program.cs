79ef8ce6741914c7d0207d0d734678c56a2e916c
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-24 21:37:48 +0300

    Наследование

---
+++ lessons_13/lessons_13/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace lessons_13
+{
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Dog : Animal
+    {
+        public float speed;
+
+        public Dog(float speed, string name) : base (name)
+        {
+            this.speed = speed;
+            Console.WriteLine("Speed " +speed);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog tom =new Dog(13.3f, "Tom");
+            Console.WriteLine(tom.Name);
+            
+            Dog tady = new Dog(23.3f, "Tady");
+            Console.WriteLine(tady.Name);
+            Console.WriteLine();
+            tady.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
