5e0809b17c430c550fe0a9766d7548f36c86047f
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-24 19:06:37 +0300

    lessons_11

---
+++ lessons_11/lessons_11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lessons_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Num is " + num);
+            }
+            catch (FormatException) {
+                Console.WriteLine("Format exception");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
