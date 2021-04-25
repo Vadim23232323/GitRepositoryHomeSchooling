7efb8fd33b05674b8e15812d4c63f98751440a0d
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-25 17:03:43 +0300

    Виртуальные операции

---
+++ lessons_15/lessons_15/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lessons_15
+{
+    class Animal
+    {
+        public string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+
+    class Dog : Animal
+    {
+        public float speed;
+
+        public Dog(float speed, string name) : base(name)
+        {
+            this.speed = speed;
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Dog Speed: "+ speed);
+        }
+    }
+
+    class Cat : Animal
+    {
+        public float speed;
+
+        public Cat(float speed, string name) : base(name)
+        {
+            this.speed = speed;
+        }
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Cat Speed: " + speed);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Dog(30, "homa"));
+            animals.Add(new Dog(10, "Charep"));
+            animals.Add(new Cat(100, "Tady"));
+
+            foreach (Animal animal in animals)
+            {
+              animal.Print();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
