cdea1a1d57900124f176c6df65c79b092aa2eb11
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-25 18:08:41 +0300

    Структуры

---
+++ lessons_20/lessons_20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lessons_20
+{
+    struct Book
+    {
+        public string title;
+        public string name;
+        public string author;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Books");
+            Book b;
+            b.author = "king";
+            Console.ReadKey();
+        }
+    }
+}
