d5005988baa1af669a740aa8c5fa8328a9a7f328
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-25 17:31:25 +0300

    Интерфеисы

---
+++ lessons_17/lessons_17/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace lessons_17
+{
+    interface ISomeInterface 
+    {
+        int property { get; set; }
+        void Print();
+    }
+
+    interface IDraw
+    {
+        void Draw();
+    }
+
+    class Test : ISomeInterface, IDraw
+    {
+        public int property { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Some text");
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        }
+    }
+
+    class Test_2: ISomeInterface
+    {
+        public int property { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Some text 2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test test = new Test();
+            Test_2 test_2 = new Test_2();
+            test.property = 2;
+            test.Print();
+            test.Draw();
+            Console.WriteLine(test.property);
+
+            Console.WriteLine("________________________________________________________");
+
+            test_2.property = 6;
+            test_2.Print();
+            Console.WriteLine(test_2.property);
+            Console.ReadKey();
+        }
+    }
+}
