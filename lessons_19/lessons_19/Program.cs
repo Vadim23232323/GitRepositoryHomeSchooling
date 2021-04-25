c42d13d431934493d80f130dcc467bd4f8ff1954
Вадим Дубовский <vadim_milo.net@mail.ru>
2021-04-25 18:02:41 +0300

    Перечисления enum

---
+++ lessons_19/lessons_19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lessons_19
+{
+    enum Breed { Dog, Cat, Bird, Bear, Tiger }
+    class  Animal
+    {
+        public Breed breed;
+        public Animal(Breed breed)
+        {
+            this.breed = breed;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(breed);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Animal(Breed.Bear);
+            dog.Print();
+            Console.ReadKey();
+        }
+    }
+}
